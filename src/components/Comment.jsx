94675de097a25ce54fbff82ffe1640a8c77b0646
Sangjunlee <pilot03067@naver.com>
2023-10-08 18:14:22 +0900

    FEAT 댓글 창 작성중

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,16 +3,53 @@ import { styled } from "styled-components";
 
 const Comment = () => {
   return (
-    <Comment1>Comment</Comment1>
+    <CommentLayout>
+      Comment
+      <CommentInputBox>
+      <CommentInput></CommentInput>
+      <CommentInputButton>입력</CommentInputButton>
+      </CommentInputBox>
+    </CommentLayout>
   )
 }
 
 export default Comment
 
-const Comment1 = styled.div`
+const CommentLayout = styled.div`
   display: flex;
   width: 1200px;
   height: 500px;
   background-color: #D9D9D9;
   margin: 100px auto 100px auto;
+  flex-direction: column;
+  gap: 10px;
+  padding: 20px;
+`;
+
+const CommentInputBox = styled.div`
+  width: 11600px;
+  height: 30px;
+  display: flex;
+  gap: 20px;
+  align-items: center;
+`;
+
+const CommentInput = styled.input`
+  width: 1000px;
+  height: 30px;
+  border-radius: 10px;
+  border: none;
+  outline: none;
+  padding-left: 15px;
+`;
+
+const CommentInputButton = styled.div`
+  width: 90px;
+  height: 30px;
+  cursor: pointer;
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  border-radius: 10px;
 `;
